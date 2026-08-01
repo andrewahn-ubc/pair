6f901e05f50631169174a4f546a610670519e40c
Andrew Ahn <andrewahn21@gmail.com>
2026-07-31 18:37:07 -0700

    fix

--- pair_adaptive_test_remainder.sh
+++ pair_adaptive_test_remainder.sh
@@ -41,7 +41,7 @@ print("\n start time: " + str(int(time.time())))
 PY
 
 IDX=$(printf "%03d" "$SLURM_ARRAY_TASK_ID")
-INPUT_PATH="${SCRATCH}/dp-llm-experiments/official_data/adaptive_test_remainder/test_${IDX}.csv"
+INPUT_PATH="${SCRATCH}/official_data/adaptive_test_remainder/test_${IDX}.csv"
 OUTPUT_PATH="${SCRATCH}/pair/results/adaptive_test_remainder_pair_merged_l2_lam0.1_eps-0.5_${IDX}.csv"
 TARGET_PATH="${SCRATCH}/merged_adaptive_l2_lam0.1_eps-0.5_ep5"
 
